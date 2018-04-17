e484ea60ab03e6962dfff87094087895b2823ca4
youngju <asdadssad@naver.coim>
2018-04-17 15:05:20 +0900

    UIAlertController

--- UIAnimate/ViewController.swift
+++ UIAnimate/ViewController.swift
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnP(_ sender: Any) {
+       let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다.", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {(myAction: UIAlertAction) -> Void in
+               self.view.backgroundColor = UIColor.red
+            })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+            })
+        let testAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+        })
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        myAlert.addAction(testAction)
+        present(myAlert, animated: true, completion: nil)
+    }
+    
 }
 
